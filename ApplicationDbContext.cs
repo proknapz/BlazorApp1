6bc6b18c25525e3c92d736222c63ced0414f8c75
conanxie <xieconan@gmail.com>
2025-03-27 11:17:52 -0500

    all class files deleted

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -12,15 +12,16 @@ public class ApplicationDbContext : DbContext
     }
     // Define DbSets for your tables (entities)
     public DbSet<Student> Students { get; set; }
-    public DbSet<StudentRecord> StudentRecords { get; set; }
-    public DbSet<AttendanceRecord> AttendanceRecords { get; set; }
-    public DbSet<Class> Classes { get; set; }
-    public DbSet<Instructor> Instructors { get; set; }
-    public DbSet<QuizResponse> Attendances { get; set; }
-    public DbSet<QuizBank> QuizBanks { get; set; }
-    public DbSet<QuizQuestion> QuizQuestions { get; set; }
-    public DbSet<QuizSession> QuizSessions { get; set; }
     
-    public DbSet<Configuration> Configurations { get; set; }
+    //public DbSet<StudentRecord> StudentRecords { get; set; }
+    //public DbSet<AttendanceRecord> AttendanceRecords { get; set; }
+    //public DbSet<Class> Classes { get; set; }
+    //public DbSet<Instructor> Instructors { get; set; }
+    //public DbSet<QuizResponse> Attendances { get; set; }
+    //public DbSet<QuizBank> QuizBanks { get; set; }
+    //public DbSet<QuizQuestion> QuizQuestions { get; set; }
+    //public DbSet<QuizSession> QuizSessions { get; set; }
+    //public DbSet<Configuration> Configurations { get; set; }
+    
     // Add more DbSet properties as needed
 }
