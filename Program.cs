f32fccb86344e3a22040c83cd224cbed1e3fd283
14699 <14699@DESKTOP-NK6LE2S>
2025-04-28 21:14:14 -0500

    add URL bypass prevent; cleanedup

--- Program.cs
+++ Program.cs
@@ -4,6 +4,9 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<UserSessionService>(); 
+
+
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
