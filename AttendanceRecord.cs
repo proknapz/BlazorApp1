bd5c5e26cb263dd37b1e815d35b510d14aa1488b
14699 <14699@DESKTOP-NK6LE2S>
2025-05-05 17:07:24 -0500

    attendancerecord updated with submit

--- AttendanceRecord.cs
+++ AttendanceRecord.cs
@@ -1,20 +1,20 @@
-using System;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 [Table("attendancerecord")]
 public class AttendanceRecord
 {
     [Key]
-    public int AttendanceId { get; set; }  // Primary Key, Auto-incremented
-    public int StudentID { get; set; }     // Foreign Key to the Student table
-    public int ClassId { get; set; }       // Foreign Key to the Class table
-    public TimeSpan ArrivalTime { get; set; } // Timestamp for the arrival time
-    public bool IsPresent { get; set; }    // Indicates if the student was present (tinyint(1))
-    public bool IsExcused { get; set; }    // Indicates if the absence was excused (tinyint(1))
-    public DateTime ClassDate { get; set; } // Date of the class
-    public string CurrentIP { get; set; }  // IP address of the student
+    public int AttendanceId { get; set; }
 
-    [ForeignKey("StudentId")]
-    public Student Student { get; set; }
+    public int StudentID { get; set; }
+    public int ClassId { get; set; }
+    public TimeSpan ArrivalTime { get; set; }
+    public bool IsPresent { get; set; }
+    public bool IsExcused { get; set; }
+    public DateTime ClassDate { get; set; }
+    public string CurrentIP { get; set; }
+
+    public Student Student { get; set; }  // ✅ no ForeignKey attribute needed
 
 }
